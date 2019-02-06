60893a9571b54c445c0c59902051e39663a2bc48
Lokiiiiii <lokeshwaran94@gmail.com>
2019-02-05 23:38:40 -0500

    +utils files

---
+++ utils.py
@@ -0,0 +1,12 @@
+import time
+
+
+def create_dir(name, parent=None):
+	"""
+	function to create directory at required depth
+	if parent exists, ./parent/name is created else ./name is created
+	You dont have to create parent
+	TODO: Yuhan
+	"""
+	now=time.time()
+	return full_path
